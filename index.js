4e7735afbe86c2c2b7a473cace561d8e97592263
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2020-03-28 23:36:06 +0800

    hbs

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const express = require('express')
-const hbs = require('express-handlebars');
+const express = require('express');
+const hbs = require('handlebars');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 app.listen(port, function(){
-    console.log("listening to port " + port);
+    console.log("Listening to http://localhost:" + port);
 });
 
-app.engine('hbs', hbs({
+app.engine('hbs', exphbs({
     extname: 'hbs',
     defaultView: 'main',
-    layoutsDir: __dirname + "/views/layouts/",
-    partialsDir: __dirname + '/views/partials/'
+	layoutsDir: path.join(__dirname, '/views/layouts'),
+	partialsDir: path.join(__dirname, '/views/partials')
 }));
 
 app.set('view engine', 'hbs');
 
+//ROUTERS
 app.get('/', function(req, res){
-    res.render('login',{ 
-        title: "Login | BucketList",
-        layout: "login"
-    })
+    res.render('landing',{ 
+        layout: "landing"
+    });
 });
 
+app.get('/profile', function(req, res){
+    res.render('profile',{ 
+        layout: "profile"
+    });
+});
