ad9c4a865f4db235927ffea291ad97c2befe64ac
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2020-04-04 00:35:38 +0800

    complete navigation
    comments and edit task needs a bit improvement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@
     function ViewComments(){
         document.getElementById("view-comments-div").style.display = "block";}
 
+    function closebtn(){
+        document.getElementById("view-comments-div").style.display = "none";}
+
 /* EDIT TASK */
     function EditTask(){
         document.getElementById("edit-task-div").style.display = "block"; }
+
+    function cancel(){
+        document.getElementById("edit-task-div").style.display = "none"; }
