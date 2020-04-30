f660279a6621f6d1b1d2c05c6c38ac01c3256ebe
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2020-05-01 03:31:25 +0800

    Update users.js

--- models/users.js
+++ models/users.js
@@ -2,18 +2,23 @@ const User = mongoose.model('user', userSchema);
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    required: true,
   },
   password: {
     type: String,
+    required: true,
   },
   displayName: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
   },
   profilePic: {
     type: String,
+    default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
   },
   bio: {
     type: String,
