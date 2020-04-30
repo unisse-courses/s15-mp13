afb3937645df0a91620984350131a3e327e6d579
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2020-05-01 03:23:58 +0800

    updated mongoose
    need: fix for models

--- app.js
+++ app.js
@@ -10,48 +10,52 @@ const port = 3000;
 
 /* DATABASE */
 /* MONGODB */
-const mongodb = require('mongodb');
-const mongoClient = mongodb.MongoClient;
-const databaseURL = "mongodb://localhost:27017/bucketlistdb";
-const dbname = "bucketlistdb";
+// const mongodb = require('mongodb');
+// const mongoClient = mongodb.MongoClient;
+// const databaseURL = "mongodb://localhost:27017/bucketlistdb";
+// const dbname = "bucketlistdb";
 
-mongoClient.connect(databaseURL, function(err, client) {
-    if (err) throw err;
-    else console.log("- bucketlistdb connected.")
-    const dbo = client.db(dbname);
+// mongoClient.connect(databaseURL, function(err, client) {
+//     if (err) throw err;
+//     else console.log("- bucketlistdb connected.")
+//     const dbo = client.db(dbname);
   
-    dbo.createCollection("users", function(err, res) {
-      if (err) throw err;
-      console.log("- users collection created.");
-      client.close();
-    });
+//     dbo.createCollection("users", function(err, res) {
+//       if (err) throw err;
+//       console.log("- users collection created.");
+//       client.close();
+//     });
 
-    dbo.createCollection("items", function(err, res) {
-        if (err) throw err;
-        console.log("- items collection created.");
-        client.close();
-    });
+//     dbo.createCollection("items", function(err, res) {
+//         if (err) throw err;
+//         console.log("- items collection created.");
+//         client.close();
+//     });
 
-    dbo.createCollection("comments", function(err, res) {
-        if (err) throw err;
-        console.log("- comments collection created.");
-        client.close();
-    });
+//     dbo.createCollection("comments", function(err, res) {
+//         if (err) throw err;
+//         console.log("- comments collection created.");
+//         client.close();
+//     });
 
-});
+// });
 
 /* MONGOOSE */
 const mongoose = require('mongoose');
-// const databaseURL = "mongodb://localhost:27017/bucketlistdb";
-const options = { useNewUrlParser: true, useUnifiedTopology : true};
+const databaseURL = "mongodb://localhost:27017/bucketlistdb";
 
-mongoose.connect(databaseURL, options);
+mongoose.connect(databaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("- bucketlistdb connected"))
+    .catch(err => console.log(err));
 
-/*  */  
+
+/* PORT */  
 app.listen(port, function(){
     console.log("Listening to http://localhost:" + port);
 });
 
+
+/* */
 app.engine('hbs', exphbs({
     extname: 'hbs',
     defaultView: 'main',
@@ -64,7 +68,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname));
 
 
-//ROUTES
+/*  */
 app.get('/', function(req, res){
     res.render('landing',{ 
         layout: 'landing'
