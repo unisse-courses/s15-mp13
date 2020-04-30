d1e1dbc1b3a1b25e5243a316f1180f4f52e45252
Kimi Catahan <61366947+annakumiko@users.noreply.github.com>
2020-04-30 11:33:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,27 +6,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-/* Skipping this part first */
+/* DATABASE STUFF */
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
 const databaseURL = "mongodb://localhost:27017/";
 const dbname = "bucketlistdb";
 
-/**
-  First connection to the database once the application starts.
-  This is so that we can create the collections needed prior to any transactions
-  that we need to do.
-**/
 mongoClient.connect(databaseURL, options, function(err, client) {
-    /**
-      Only do database manipulation inside of the connection
-      When a connection is made, it will try to make the database
-      automatically. The collection(like a table) needs to be made.
-    **/
     if (err) throw err;
     const dbo = client.db(dbname);
   
-    //Will create a collection if it has not yet been made
     dbo.createCollection("list", function(err, res) {
       if (err) throw err;
       console.log("Collection created!");
